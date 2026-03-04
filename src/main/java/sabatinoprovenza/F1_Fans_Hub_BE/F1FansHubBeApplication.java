b81e9d6cf5549472af1978e867ee102e9fa53273
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-03-04 12:17:00 +0100

    Initial commit

---
+++ src/main/java/sabatinoprovenza/F1_Fans_Hub_BE/F1FansHubBeApplication.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.F1_Fans_Hub_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1FansHubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1FansHubBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sabatinoprovenza/F1_Fans_Hub_BE/F1FansHubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.F1_Fans_Hub_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1FansHubBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
